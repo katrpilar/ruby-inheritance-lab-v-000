0054c1649c7aeeaf6afe37d16d7607ea4b587a73
Katrina Brinson <kbrinso@gmail.com>
2018-02-02 22:48:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,10 @@ require './lib/user.rb'
 class Student < User
   
   def initialize
-    @knoweledge = []
+    @knowledge = []
   end
-
+"takes in an argument of a string of knowledge and adds it to the student's knowledge array"
+  def learn(str)
+    @knowledge << str
+  end
 end
